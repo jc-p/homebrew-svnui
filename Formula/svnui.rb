83467c3d07f7912835376620e6c30df7e99b8098
彭杰程 <pengjiecheng@szkingdom.com>
2026-09-15 17:22:01 +0800

    更新
    移除了所有 if OS.arch == ...，改用 Homebrew 标准的 on_arm 块。
    
    顶层保留了默认的 url 和 sha256，这样即使在不满足 on_arm 条件的平台上（如
    Linux x86_64），也能解析到一个 URL，readall 就不会报错。
    
    on_arm 块内的 url 和 sha256 会覆盖顶层的值，所以 ARM Mac 会正确下载
    aarch64 版本。

--- Formula/svnui.rb
+++ Formula/svnui.rb
@@ -3,21 +3,21 @@ class Svnui < Formula
   homepage "https://github.com/jc-p/svnui"
   license "MIT"
 
-  # 使用 if: OS.arch 来为不同架构指定不同的下载链接
-  # 这是 Homebrew 官方推荐的写法，可以确保解析器能正确识别到 url 字段
-  url "https://github.com/jc-p/svnui/releases/download/0.1.1/svnui-0.1.1-aarch64-apple-darwin.tar.gz" if OS.arch == :arm64
-  url "https://github.com/jc-p/svnui/releases/download/0.1.1/svnui-0.1.1-x86_64-apple-darwin.tar.gz" if OS.arch == :x86_64
+  # 默认 URL（这里用 x86_64 的，作为所有平台的回退）
+  url "https://github.com/jc-p/svnui/releases/download/0.1.1/svnui-0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "2a5666f5163aa9c7752a0fe24e9ed3f0140de5babe647c01217a632e76638d32"
 
-  # 同样，sha256 也需要对应修改
-  sha256 "a343d2c7e74349aa42b1aa62433ec5830dfa3ea42ac6b4b6d004df158d6cfa4b" if OS.arch == :arm64
-  sha256 "2a5666f5163aa9c7752a0fe24e9ed3f0140de5babe647c01217a632e76638d32" if OS.arch == :x86_64
+  # 针对 ARM 架构覆盖 URL 和 sha256
+  on_arm do
+    url "https://github.com/jc-p/svnui/releases/download/0.1.1/svnui-0.1.1-aarch64-apple-darwin.tar.gz"
+    sha256 "a343d2c7e74349aa42b1aa62433ec5830dfa3ea42ac6b4b6d004df158d6cfa4b"
+  end
 
   def install
     bin.install "svnui"
   end
 
   test do
-    # --version 由 clap 的 #[command(version)] 提供
     assert_match "0.1.1", shell_output("#{bin}/svnui --version")
   end
 end
